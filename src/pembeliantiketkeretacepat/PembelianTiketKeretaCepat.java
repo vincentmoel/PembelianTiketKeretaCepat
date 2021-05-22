405969e59bd57e75d86544ffcae55c0a25adc525
Vincent Nathaniel M <vincentmoeljopranoto@gmail.com>
2021-05-22 17:50:16 +0700

    GUI Form v.1

--- src/pembeliantiketkeretacepat/PembelianTiketKeretaCepat.java
+++ src/pembeliantiketkeretacepat/PembelianTiketKeretaCepat.java
@@ -14,8 +14,5 @@ public class PembelianTiketKeretaCepat {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+
 }
